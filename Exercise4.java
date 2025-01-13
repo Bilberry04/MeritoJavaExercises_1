94326875e3a267bc7242e814cc4fe9d80eed0002
Bilberry04 <kacper.borowiec04@gmail.com>
2025-01-13 18:23:07 +0100

    Update Exercise4.java

--- Exercise4.java
+++ Exercise4.java
@@ -1,15 +1,3 @@
-//Zadanie 6. Napisać program realizujący funkcje prostego kalkulatora,
-//pozwalającego na wykonywanie operacji dodawania, odejmowania,
-//mnożenia i dzielenia na dwóch liczbach rzeczywistych. Program ma
-//identyfikować sytuację wprowadzenia błędnego symbolu działania
-//oraz próbę dzielenia przez zero. Zastosować instrukcję switch do
-//wykonania odpowiedniego działania w zależności od
-//wprowadzonego symbolu operacji. Scenariusz działania programu:
-//a) Program wyświetla informację o swoim przeznaczeniu.
-//b) Wczytuje pierwszą liczbę.
-//c) Wczytuje symbol operacji arytmetycznej: +, -, *, /.
-//d) Wczytuje drugą liczbę.
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
