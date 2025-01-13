c2005cc04f63daeda058649b26d46846742dbfc4
Bilberry04 <kacper.borowiec04@gmail.com>
2025-01-13 18:22:22 +0100

    Update Exercise7.java

--- Exercise7.java
+++ Exercise7.java
@@ -1,8 +1,3 @@
-package Exercises;//Zadanie 7. Napisać program, który pobiera od użytkownika liczbę
-//całkowitą dodatnią, a na- stępnie wyświetla na ekranie kolejno
-//wszystkie liczby niepatrzyste nie większe od podanej liczby.
-//Przykład, dla 15 program powinien wyświetlić 1, 3, 5, 7, 9, 11, 13, 15.
-
 public class Exercise7 {
     public static void main(String[] args) {
         int number = Utils.inputIntStatic("Podaj liczbę: ");
