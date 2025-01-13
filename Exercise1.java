675e67c96b006670df8bce821732c39a9cfcf587
Bilberry04 <kacper.borowiec04@gmail.com>
2025-01-13 18:23:35 +0100

    Update Exercise1.java

--- Exercise1.java
+++ Exercise1.java
@@ -1,7 +1,3 @@
-//Zadanie 1. Napisać program służący do konwersji wartości
-//temperatury podanej w stopniach Celsjusza na stopnie w skali
-//Fahrenheita (stopnie Fahrenheita = 1.8 * stopnie Celsjusza + 32.0)
-
 import java.util.Scanner;
 
 public class Exercise1 {
