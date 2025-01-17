bc246325b61c6f669909f79c45ba04f1251c6edc
Bilberry04 <kacper.borowiec04@gmail.com>
2025-01-17 11:10:02 +0100

    Update Exercise1.java

--- Exercise1.java
+++ Exercise1.java
@@ -2,16 +2,26 @@ import java.util.Scanner;
 
 public class Exercise1 {
     public static void main(String[] args) {
+       
         final double MULTIPLIER = 1.8;
         final double OFFSET = 32;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Podaj temperaturę w stopniach Celsjusza: ");
-        double celsius = scanner.nextDouble();
-        scanner.close();
+        try {
+            System.out.print("Podaj temperaturę w stopniach Celsjusza: ");
+           
+            if (scanner.hasNextDouble()) {
+                double celsius = scanner.nextDouble();
 
-        double fahrenheit = (celsius * MULTIPLIER) + OFFSET;
+                double fahrenheit = (celsius * MULTIPLIER) + OFFSET;
 
-        System.out.println(celsius + " stopni Celsjusza to " + fahrenheit + " stopni Fahrenheita.");
+                System.out.printf("%.2f stopni Celsjusza to %.2f stopni Fahrenheita.%n", celsius, fahrenheit);
+            } else {
+                System.out.println("Nieprawidłowy format liczby. Proszę podać liczbę.");
+            }
+        } finally {
+           
+            scanner.close();
+        }
     }
 }
