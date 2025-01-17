58310e55139cca3eb8d63b057e910636c5257dc8
Bilberry04 <kacper.borowiec04@gmail.com>
2025-01-17 11:10:45 +0100

    Update Exercise2.java

--- Exercise2.java
+++ Exercise2.java
@@ -3,32 +3,27 @@ import java.util.Scanner;
 
 public class Exercise2 {
     public static void main(String[] args) {
+        int a = inputInt("Podaj pierwszą liczbę: ");
+        int b = inputInt("Podaj drugą liczbę: ");
+        int c = inputInt("Podaj trzecią liczbę: ");
 
-        int a = Utils.inputIntStatic("Podaj pierwszą liczbę: ");
-        int b = Utils.inputIntStatic("Podaj drugą liczbę: ");
-        int c = Utils.inputIntStatic("Podaj trzecią liczbę: ");
-
-        int max;
-        int min;
-
-        if (a > b && a > c) {
-            max = a;
-        } else if (b > a && b > c) {
-            max = b;
-        } else {
-            max = c;
-        }
-
-        if (a < b && a < c) {
-            min = a;
-        } else if (b < a && b < c) {
-            min = b;
-        } else {
-            min = c;
-        }
+        int max = Math.max(a, Math.max(b, c));
+        int min = Math.min(a, Math.min(b, c));
 
         System.out.println("Największa liczba to: " + max);
         System.out.println("Najmniejsza liczba to: " + min);
+    }
 
+    private static int inputInt(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print(prompt);
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Nieprawidłowe dane wejściowe. Proszę podać liczbę całkowitą.");
+                scanner.next(); 
+            }
+        }
     }
 }
