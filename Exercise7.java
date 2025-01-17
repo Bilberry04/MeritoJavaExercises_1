0a443a430c3a2eedff38af401b395c216876cdb4
Bilberry04 <kacper.borowiec04@gmail.com>
2025-01-17 11:12:28 +0100

    Update Exercise7.java

--- Exercise7.java
+++ Exercise7.java
@@ -1,18 +1,33 @@
+import java.util.Scanner;
+
 public class Exercise7 {
     public static void main(String[] args) {
-        int number = Utils.inputIntStatic("Podaj liczbę: ");
+        int number = inputInt("Podaj liczbę: ");
 
         if (number < 0) {
             System.out.println("Podana liczba jest liczbą ujemną");
         } else {
-            for (int i = 0; i <= number; i++) {
-                if (i % 2 != 0) {
-                    if (i < number) {
-                        System.out.print(i + ", ");
-                    } else {
-                        System.out.print(i + ".");
-                    }
+            System.out.print("Liczby nieparzyste: ");
+            for (int i = 1; i <= number; i += 2) {
+                if (i < number - 1) {
+                    System.out.print(i + ", ");
+                } else {
+                    System.out.print(i);
                 }
+            }
+            System.out.println(".");
+        }
+    }
+
+    private static int inputInt(String message) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print(message);
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Nieprawidłowe dane wejściowe. Proszę podać liczbę całkowitą.");
+                scanner.next(); 
             }
         }
     }
