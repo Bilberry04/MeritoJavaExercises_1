f51411816c718c4f5d29bfdf4ab627771e29d08d
Bilberry04 <kacper.borowiec04@gmail.com>
2025-01-17 11:11:38 +0100

    Update Exercise3.java

--- Exercise3.java
+++ Exercise3.java
@@ -6,7 +6,7 @@ public class Exercise3 {
         System.out.println("Kalkulator ratalny");
 
         double price = inputDouble("Podaj kwotę: ");
-        int rate = Utils.inputIntStatic("Podaj liczbę rat: ");
+        int rate = inputInt("Podaj liczbę rat: ");
 
         printSeparator();
 
@@ -26,9 +26,9 @@ public class Exercise3 {
         double total = price + interest;
         double rateValue = total / rate;
 
-        System.out.println("Odsetki: " + interest + "zł");
-        System.out.println("Łączny koszt: " + total + "zł");
-        System.out.println("Wysokość raty: " + rateValue + "zł");
+        System.out.printf("Odsetki: %.2f zł%n", interest);
+        System.out.printf("Łączny koszt: %.2f zł%n", total);
+        System.out.printf("Wysokość raty: %.2f zł%n", rateValue);
     }
 
     private static double getMultiplier(int rate) {
@@ -47,18 +47,33 @@ public class Exercise3 {
 
     private static void showError() {
         System.out.println("Podano błędną wartość !!!");
-        System.out.println("Kwota powinna znajdować się w przedziale od 100zł do 10 000zł");
+        System.out.println("Kwota powinna znajdować się w przedziale od 100 zł do 10 000 zł");
         System.out.println("Rata powinna znajdować się w przedziale od 6 do 48");
     }
 
-    static double inputDouble(String message) {
-        try {
-            System.out.print(message);
-            Scanner scanner = new Scanner(System.in);
-            return scanner.nextDouble();
-        } catch (InputMismatchException e) {
-            System.out.println("Podano błędną wartość");
-            return inputDouble(message);
+    private static double inputDouble(String message) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print(message);
+                return scanner.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Nieprawidłowe dane wejściowe. Proszę podać liczbę.");
+                scanner.next(); 
+            }
+        }
+    }
+
+    private static int inputInt(String message) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print(message);
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Nieprawidłowe dane wejściowe. Proszę podać liczbę całkowitą.");
+                scanner.next(); 
+            }
         }
     }
 }
